e1623ce9c76e4d6773e7479dab8241b693b5589a
lollygagger <ubercharge60@gmail.com>
2023-02-27 09:27:25 -0500

    repushed prisma trying to fix deploy issue

--- src/pages/api/classmates.ts
+++ src/pages/api/classmates.ts
@@ -10,33 +10,33 @@ type Data = {
     university: string
 }
 
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//
-//     const prisma = new PrismaClient()
-//
-//     /**
-//      * Post endpoint for /api/classmate
-//      *
-//      * req body contains name, major, university
-//      */
-//     if(req.method==="POST"){
-//         const {name, major, university}: Data = req.body
-//
-//         prisma.profile.create({
-//             data: {
-//                 name: name,
-//                 major: major,
-//                 university: university
-//             }
-//         })
-//         }
-//
-//     //gets a list of all profiles from db
-//     if(req.method==="GET"){
-//         const classmates = await prisma.profile.findMany()
-//
-//         if(classmates !== null){
-//             res.status(200).json({Result: classmates})
-//         }
-//     }
-// }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+    const prisma = new PrismaClient()
+
+    /**
+     * Post endpoint for /api/classmate
+     *
+     * req body contains name, major, university
+     */
+    if(req.method==="POST"){
+        const {name, major, university}: Data = req.body
+
+        prisma.profile.create({
+            data: {
+                name: name,
+                major: major,
+                university: university
+            }
+        })
+        }
+
+    //gets a list of all profiles from db
+    if(req.method==="GET"){
+        const classmates = await prisma.profile.findMany()
+
+        if(classmates !== null){
+            res.status(200).json({Result: classmates})
+        }
+    }
+}
